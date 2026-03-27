386ac95654a020428b241626b39db36c716729da
tejas-su <tejas_s.u@yahoo.com>
2026-03-27 22:01:33 +0530

    fix: Removed experimental in next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    allowedDevOrigins: ['10.246.126.100'],
-  },
-  allowedDevOrigins: ['10.246.126.100'],
 }
 
 export default nextConfig
